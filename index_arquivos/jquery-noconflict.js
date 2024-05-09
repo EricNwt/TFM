f2243ea6ff2e3a3c32265fce2827f86f0ae774b7
EricNwt <skilobaloba@gmail.com>
2024-05-09 15:29:13 -0300

    Primeiro Comit

---
+++ index_arquivos/jquery-noconflict.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+jQuery.noConflict();
+
+
+}
+/*
+     FILE ARCHIVED ON 22:20:15 May 09, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:22:08 May 08, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.844
+  exclusion.robots: 0.085
+  exclusion.robots.policy: 0.071
+  esindex: 0.016
+  cdx.remote: 6.241
+  LoadShardBlock: 71.67 (3)
+  PetaboxLoader3.datanode: 47.446 (4)
+  PetaboxLoader3.resolve: 106.621 (2)
+  load_resource: 119.152
+*/
